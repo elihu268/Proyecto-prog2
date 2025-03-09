69916ec67149ebc481d9d591c3ec22de56421ad8
YadhiraAna <yayakim01412@gmail.com>
2025-03-09 15:27:53 -0600

    cascarond e compra y permisos

--- Sistema_Ventas/View/frmAsignarPermisos.Designer.cs
+++ Sistema_Ventas/View/frmAsignarPermisos.Designer.cs
@@ -28,50 +28,113 @@
         /// </summary>
         private void InitializeComponent()
         {
-            btn_actualizar = new Button();
-            lbl_indique = new Label();
-            btn_seleccionar = new Button();
-            textBox1 = new TextBox();
+            splitContainer1 = new SplitContainer();
+            groupBox1 = new GroupBox();
+            groupBox2 = new GroupBox();
+            btn_editar = new Button();
+            dataGridView1 = new DataGridView();
+            clm_usuario = new DataGridViewTextBoxColumn();
+            clm_rol = new DataGridViewTextBoxColumn();
+            clm_estatus = new DataGridViewTextBoxColumn();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).BeginInit();
+            splitContainer1.Panel1.SuspendLayout();
+            splitContainer1.Panel2.SuspendLayout();
+            splitContainer1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
-            // btn_actualizar
+            // splitContainer1
             // 
-            btn_actualizar.BackColor = SystemColors.ActiveCaption;
-            btn_actualizar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_actualizar.Location = new Point(719, 301);
-            btn_actualizar.Name = "btn_actualizar";
-            btn_actualizar.Size = new Size(150, 69);
-            btn_actualizar.TabIndex = 0;
-            btn_actualizar.Text = "Actualizar permisos";
-            btn_actualizar.UseVisualStyleBackColor = false;
+            splitContainer1.Location = new Point(-2, 1);
+            splitContainer1.Name = "splitContainer1";
             // 
-            // lbl_indique
+            // splitContainer1.Panel1
             // 
-            lbl_indique.AutoSize = true;
-            lbl_indique.Location = new Point(21, 13);
-            lbl_indique.Name = "lbl_indique";
-            lbl_indique.Size = new Size(153, 25);
-            lbl_indique.TabIndex = 1;
-            lbl_indique.Text = "Ingrese id usuario";
+            splitContainer1.Panel1.Controls.Add(groupBox1);
             // 
-            // btn_seleccionar
+            // splitContainer1.Panel2
             // 
-            btn_seleccionar.BackColor = SystemColors.ActiveCaption;
-            btn_seleccionar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btn_seleccionar.Location = new Point(213, 40);
-            btn_seleccionar.Name = "btn_seleccionar";
-            btn_seleccionar.Size = new Size(150, 46);
-            btn_seleccionar.TabIndex = 2;
-            btn_seleccionar.Text = "Seleccionar";
-            btn_seleccionar.UseVisualStyleBackColor = false;
-            btn_seleccionar.Click += btn_seleccionar_Click;
+            splitContainer1.Panel2.Controls.Add(groupBox2);
+            splitContainer1.Size = new Size(899, 472);
+            splitContainer1.SplitterDistance = 299;
+            splitContainer1.TabIndex = 6;
             // 
-            // textBox1
+            // groupBox1
             // 
-            textBox1.Location = new Point(24, 55);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(150, 31);
-            textBox1.TabIndex = 3;
+            groupBox1.Controls.Add(button1);
+            groupBox1.Location = new Point(5, 11);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(291, 458);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(btn_editar);
+            groupBox2.Controls.Add(dataGridView1);
+            groupBox2.Location = new Point(15, 15);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(578, 454);
+            groupBox2.TabIndex = 11;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "groupBox2";
+            // 
+            // btn_editar
+            // 
+            btn_editar.BackColor = SystemColors.ActiveCaption;
+            btn_editar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_editar.Location = new Point(63, 62);
+            btn_editar.Name = "btn_editar";
+            btn_editar.Size = new Size(150, 46);
+            btn_editar.TabIndex = 15;
+            btn_editar.Text = "Editar";
+            btn_editar.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { clm_usuario, clm_rol, clm_estatus });
+            dataGridView1.Location = new Point(30, 114);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 62;
+            dataGridView1.Size = new Size(516, 225);
+            dataGridView1.TabIndex = 14;
+            // 
+            // clm_usuario
+            // 
+            clm_usuario.HeaderText = "Usuario";
+            clm_usuario.MinimumWidth = 8;
+            clm_usuario.Name = "clm_usuario";
+            clm_usuario.Width = 150;
+            // 
+            // clm_rol
+            // 
+            clm_rol.HeaderText = "Rol";
+            clm_rol.MinimumWidth = 8;
+            clm_rol.Name = "clm_rol";
+            clm_rol.Width = 150;
+            // 
+            // clm_estatus
+            // 
+            clm_estatus.HeaderText = "Estatus";
+            clm_estatus.MinimumWidth = 8;
+            clm_estatus.Name = "clm_estatus";
+            clm_estatus.Width = 150;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ActiveCaption;
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.Location = new Point(39, 360);
+            button1.Name = "button1";
+            button1.Size = new Size(153, 59);
+            button1.TabIndex = 1;
+            button1.Text = "Actualizar permisos";
+            button1.UseVisualStyleBackColor = false;
             // 
             // frmAsignarPermisos
             // 
@@ -79,21 +142,29 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.GradientInactiveCaption;
             ClientSize = new Size(909, 485);
-            Controls.Add(textBox1);
-            Controls.Add(btn_seleccionar);
-            Controls.Add(lbl_indique);
-            Controls.Add(btn_actualizar);
+            Controls.Add(splitContainer1);
             Name = "frmAsignarPermisos";
             Text = "Permisos";
+            splitContainer1.Panel1.ResumeLayout(false);
+            splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)splitContainer1).EndInit();
+            splitContainer1.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
-        private Button btn_actualizar;
-        private Label lbl_indique;
-        private Button btn_seleccionar;
-        private TextBox textBox1;
+        private SplitContainer splitContainer1;
+        private GroupBox groupBox1;
+        private Button button1;
+        private GroupBox groupBox2;
+        private Button btn_editar;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn clm_usuario;
+        private DataGridViewTextBoxColumn clm_rol;
+        private DataGridViewTextBoxColumn clm_estatus;
     }
 }
--- Sistema_Ventas/View/frmVenta.Designer.cs
+++ Sistema_Ventas/View/frmVenta.Designer.cs
@@ -30,13 +30,32 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmVenta));
             sc_venta = new SplitContainer();
+            grp_busqueda = new GroupBox();
             gbx_venta = new GroupBox();
             gbx_metodo = new GroupBox();
-            groupBox1 = new GroupBox();
+            dtv_productos = new DataGridView();
+            clm_id = new DataGridViewTextBoxColumn();
+            clm_nombre = new DataGridViewTextBoxColumn();
+            clm_precio = new DataGridViewTextBoxColumn();
+            clm_stock = new DataGridViewTextBoxColumn();
+            btn_terminar = new Button();
+            btn_limpiar = new Button();
+            lbl_nombre = new Label();
+            lbl_cantidad = new Label();
+            textBox1 = new TextBox();
+            nud_cantidad = new NumericUpDown();
+            btn_agregar = new Button();
+            btn_eliminar = new Button();
+            grbox_carrito = new GroupBox();
             ((System.ComponentModel.ISupportInitialize)sc_venta).BeginInit();
             sc_venta.Panel1.SuspendLayout();
             sc_venta.Panel2.SuspendLayout();
             sc_venta.SuspendLayout();
+            grp_busqueda.SuspendLayout();
+            gbx_venta.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dtv_productos).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)nud_cantidad).BeginInit();
+            grbox_carrito.SuspendLayout();
             SuspendLayout();
             // 
             // sc_venta
@@ -47,51 +66,189 @@
             // 
             // sc_venta.Panel1
             // 
-            sc_venta.Panel1.Controls.Add(groupBox1);
+            sc_venta.Panel1.Controls.Add(grbox_carrito);
+            sc_venta.Panel1.Controls.Add(btn_terminar);
+            sc_venta.Panel1.Controls.Add(dtv_productos);
+            sc_venta.Panel1.Controls.Add(grp_busqueda);
             sc_venta.Panel1.Controls.Add(gbx_venta);
             // 
             // sc_venta.Panel2
             // 
             sc_venta.Panel2.Controls.Add(gbx_metodo);
             sc_venta.Panel2.Paint += splitContainer1_Panel2_Paint;
-            sc_venta.Size = new Size(1115, 636);
-            sc_venta.SplitterDistance = 762;
+            sc_venta.Size = new Size(1452, 636);
+            sc_venta.SplitterDistance = 992;
             sc_venta.TabIndex = 0;
+            // 
+            // grp_busqueda
+            // 
+            grp_busqueda.Controls.Add(textBox1);
+            grp_busqueda.Location = new Point(3, 3);
+            grp_busqueda.Name = "grp_busqueda";
+            grp_busqueda.Size = new Size(986, 86);
+            grp_busqueda.TabIndex = 1;
+            grp_busqueda.TabStop = false;
+            grp_busqueda.Text = "busqueda";
             // 
             // gbx_venta
             // 
-            gbx_venta.Location = new Point(3, 95);
+            gbx_venta.Controls.Add(btn_agregar);
+            gbx_venta.Controls.Add(nud_cantidad);
+            gbx_venta.Controls.Add(lbl_cantidad);
+            gbx_venta.Controls.Add(lbl_nombre);
+            gbx_venta.Location = new Point(12, 95);
             gbx_venta.Name = "gbx_venta";
-            gbx_venta.Size = new Size(751, 78);
+            gbx_venta.Size = new Size(977, 105);
             gbx_venta.TabIndex = 0;
             gbx_venta.TabStop = false;
-            gbx_venta.Text = "Busqueda";
+            gbx_venta.Text = "datos articulo";
             // 
             // gbx_metodo
             // 
             gbx_metodo.Location = new Point(11, 10);
             gbx_metodo.Name = "gbx_metodo";
-            gbx_metodo.Size = new Size(335, 623);
+            gbx_metodo.Size = new Size(433, 623);
             gbx_metodo.TabIndex = 0;
             gbx_metodo.TabStop = false;
             gbx_metodo.Text = "Metodo de pago";
             gbx_metodo.Enter += gbx_metodo_Enter;
             // 
-            // groupBox1
+            // dtv_productos
             // 
-            groupBox1.Location = new Point(3, 3);
-            groupBox1.Name = "groupBox1";
-            groupBox1.Size = new Size(756, 86);
-            groupBox1.TabIndex = 1;
-            groupBox1.TabStop = false;
-            groupBox1.Text = "groupBox1";
+            dtv_productos.AllowUserToAddRows = false;
+            dtv_productos.AllowUserToDeleteRows = false;
+            dtv_productos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dtv_productos.Columns.AddRange(new DataGridViewColumn[] { clm_id, clm_nombre, clm_precio, clm_stock });
+            dtv_productos.Location = new Point(41, 250);
+            dtv_productos.Name = "dtv_productos";
+            dtv_productos.RowHeadersWidth = 62;
+            dtv_productos.Size = new Size(606, 225);
+            dtv_productos.TabIndex = 2;
+            // 
+            // clm_id
+            // 
+            clm_id.HeaderText = "Id";
+            clm_id.MinimumWidth = 8;
+            clm_id.Name = "clm_id";
+            clm_id.Width = 150;
+            // 
+            // clm_nombre
+            // 
+            clm_nombre.HeaderText = "Nombre";
+            clm_nombre.MinimumWidth = 8;
+            clm_nombre.Name = "clm_nombre";
+            clm_nombre.Width = 150;
+            // 
+            // clm_precio
+            // 
+            clm_precio.HeaderText = "precio";
+            clm_precio.MinimumWidth = 8;
+            clm_precio.Name = "clm_precio";
+            clm_precio.Width = 150;
+            // 
+            // clm_stock
+            // 
+            clm_stock.HeaderText = "Strock";
+            clm_stock.MinimumWidth = 8;
+            clm_stock.Name = "clm_stock";
+            clm_stock.Width = 150;
+            // 
+            // btn_terminar
+            // 
+            btn_terminar.BackColor = SystemColors.ActiveCaption;
+            btn_terminar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_terminar.Location = new Point(287, 590);
+            btn_terminar.Name = "btn_terminar";
+            btn_terminar.Size = new Size(195, 34);
+            btn_terminar.TabIndex = 3;
+            btn_terminar.Text = "Terminar Compra";
+            btn_terminar.UseVisualStyleBackColor = false;
+            // 
+            // btn_limpiar
+            // 
+            btn_limpiar.BackColor = SystemColors.ActiveCaption;
+            btn_limpiar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_limpiar.Location = new Point(55, 321);
+            btn_limpiar.Name = "btn_limpiar";
+            btn_limpiar.Size = new Size(195, 34);
+            btn_limpiar.TabIndex = 4;
+            btn_limpiar.Text = "limpiar";
+            btn_limpiar.UseVisualStyleBackColor = false;
+            // 
+            // lbl_nombre
+            // 
+            lbl_nombre.AutoSize = true;
+            lbl_nombre.Location = new Point(19, 30);
+            lbl_nombre.Name = "lbl_nombre";
+            lbl_nombre.Size = new Size(84, 25);
+            lbl_nombre.TabIndex = 0;
+            lbl_nombre.Text = "nombre: ";
+            // 
+            // lbl_cantidad
+            // 
+            lbl_cantidad.AutoSize = true;
+            lbl_cantidad.Location = new Point(163, 29);
+            lbl_cantidad.Name = "lbl_cantidad";
+            lbl_cantidad.Size = new Size(80, 25);
+            lbl_cantidad.TabIndex = 1;
+            lbl_cantidad.Text = "cantidad";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(38, 49);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(150, 31);
+            textBox1.TabIndex = 2;
+            // 
+            // nud_cantidad
+            // 
+            nud_cantidad.Location = new Point(167, 61);
+            nud_cantidad.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            nud_cantidad.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
+            nud_cantidad.Name = "nud_cantidad";
+            nud_cantidad.Size = new Size(180, 31);
+            nud_cantidad.TabIndex = 2;
+            nud_cantidad.Value = new decimal(new int[] { 1, 0, 0, 0 });
+            // 
+            // btn_agregar
+            // 
+            btn_agregar.BackColor = SystemColors.ActiveCaption;
+            btn_agregar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_agregar.Location = new Point(427, 65);
+            btn_agregar.Name = "btn_agregar";
+            btn_agregar.Size = new Size(91, 34);
+            btn_agregar.TabIndex = 4;
+            btn_agregar.Text = "Agregar";
+            btn_agregar.UseVisualStyleBackColor = false;
+            // 
+            // btn_eliminar
+            // 
+            btn_eliminar.BackColor = SystemColors.ActiveCaption;
+            btn_eliminar.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_eliminar.Location = new Point(55, 267);
+            btn_eliminar.Name = "btn_eliminar";
+            btn_eliminar.Size = new Size(195, 34);
+            btn_eliminar.TabIndex = 5;
+            btn_eliminar.Text = "eliminar";
+            btn_eliminar.UseVisualStyleBackColor = false;
+            // 
+            // grbox_carrito
+            // 
+            grbox_carrito.Controls.Add(btn_eliminar);
+            grbox_carrito.Controls.Add(btn_limpiar);
+            grbox_carrito.Location = new Point(671, 202);
+            grbox_carrito.Name = "grbox_carrito";
+            grbox_carrito.Size = new Size(300, 410);
+            grbox_carrito.TabIndex = 5;
+            grbox_carrito.TabStop = false;
+            grbox_carrito.Text = "Carrito de compras";
             // 
             // frmVenta
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.GradientInactiveCaption;
-            ClientSize = new Size(1115, 636);
+            ClientSize = new Size(1452, 636);
             Controls.Add(sc_venta);
             Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "frmVenta";
@@ -101,6 +258,13 @@
             sc_venta.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)sc_venta).EndInit();
             sc_venta.ResumeLayout(false);
+            grp_busqueda.ResumeLayout(false);
+            grp_busqueda.PerformLayout();
+            gbx_venta.ResumeLayout(false);
+            gbx_venta.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dtv_productos).EndInit();
+            ((System.ComponentModel.ISupportInitialize)nud_cantidad).EndInit();
+            grbox_carrito.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -109,6 +273,20 @@
         private SplitContainer sc_venta;
         private GroupBox gbx_venta;
         private GroupBox gbx_metodo;
-        private GroupBox groupBox1;
+        private GroupBox grp_busqueda;
+        private DataGridView dtv_productos;
+        private DataGridViewTextBoxColumn clm_id;
+        private DataGridViewTextBoxColumn clm_nombre;
+        private DataGridViewTextBoxColumn clm_precio;
+        private DataGridViewTextBoxColumn clm_stock;
+        private Button btn_terminar;
+        private Button btn_limpiar;
+        private TextBox textBox1;
+        private NumericUpDown nud_cantidad;
+        private Label lbl_cantidad;
+        private Label lbl_nombre;
+        private GroupBox grbox_carrito;
+        private Button btn_eliminar;
+        private Button btn_agregar;
     }
 }
