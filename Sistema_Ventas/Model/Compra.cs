03c6d2b2969f52efdf0dc5c13ae8cc8f45b77c1d
YadhiraAna <yayakim01412@gmail.com>
2025-04-19 17:53:37 -0600

    c

--- Sistema_Ventas/Model/Compra.cs
+++ Sistema_Ventas/Model/Compra.cs
@@ -14,10 +14,23 @@ namespace Sistema_Ventas.Model
         public int MetodoPago { get; set; }
         public double Iva { get; set; }
         public double Subtotal { get; set; }
-        public int Estatus { get; set; }
         public double Descuento { get; set; }
         public int Total { get; set; }
-
         public DateTime FechaCompra { get; set; }
+        public int Estatus { get; set; }
+
+        public Compra(int id_compra,int id_cliente,string codigo,int metodo_de_pago,double iva,double subtotal,double descuento,int total,DateTime fecha_de_compra, int estatus)
+        {
+            this.IdCompra = id_compra;
+            this.IdCliente = id_cliente;
+            this.Codigo = codigo;
+            this.MetodoPago = metodo_de_pago;
+            this.Iva = iva;
+            this.Subtotal = subtotal;
+            this.Descuento = descuento;
+            this.Total = total;
+            this.FechaCompra = fecha_de_compra;
+            this.Estatus = estatus;
+        }
     }
 }
