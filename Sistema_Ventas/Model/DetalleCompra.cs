ecf04ea8fcaa404eb47d38b35708b4e660333409
YadhiraAna <yayakim01412@gmail.com>
2025-04-19 18:00:54 -0600

    c

--- Sistema_Ventas/Model/DetalleCompra.cs
+++ Sistema_Ventas/Model/DetalleCompra.cs
@@ -3,21 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NLog.LayoutRenderers;
 
 namespace Sistema_Ventas.Model
 {
     public class DetalleCompra
     {
-        public int IdCompra { get; set; }
-
         public int IdDetalle { get; set; }
-
+        public int IdCompra { get; set; }
         public int IdProducto { get; set; }
 
         public int cantidad { get; set; }
         public double TotalPorUnidad { get; set; }
         //constructor
-
+        public DetalleCompra(int id_detalle, int id_compra,int id_producto,int cantidad, double total_por_unidad
+) { 
+            this.IdDetalle = id_detalle;
+            this.IdCompra = id_compra;
+            this.IdProducto = id_producto;
+            this.cantidad = cantidad;
+            this.TotalPorUnidad = total_por_unidad;
+        }
 
        
     }
--- Sistema_Ventas/Model/Producto.cs
+++ Sistema_Ventas/Model/Producto.cs
@@ -10,7 +10,7 @@ namespace Sistema_Ventas.Model
     public class Producto
     {
         public int IdProduco { get; set; }
-        public string IdCodigo { get; set; }
+        public string Codigo { get; set; }
         public string Nombre { get; set; }
 
         public double Precio { get; set; }
@@ -20,7 +20,7 @@ namespace Sistema_Ventas.Model
 
         public Producto(int idProducto,string codigo,string nombre,double precio,string  descripcion, int existencia) {
         this.IdProduco = idProducto;
-            this.IdCodigo = codigo; 
+            this.Codigo = codigo; 
             this.Nombre = nombre;
             this.Precio = precio;
             this.Descripcion = descripcion;
