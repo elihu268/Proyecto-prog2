c9b764d9afe5f0a7f64ef9c0206ef7fd7e23de5a
Alan <witrifido54@hotmail.com>
2025-04-25 13:15:49 -0600

    model Auditorias

--- Sistema_Ventas/Model/Auditoria.cs
+++ Sistema_Ventas/Model/Auditoria.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Sistema_Ventas.Model
 {
-    internal class Auditoria
+    class Auditoria
     {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Accion { get; set; }
+        public DateTime FechaHora { get; set; }
+        public string Detalles { get; set; }
+        public Auditoria(int id, string usuario, string accion, DateTime fechaHora, string detalles)
+        {
+            Id = id;
+            Usuario = usuario;
+            Accion = accion;
+            FechaHora = fechaHora;
+            Detalles = detalles;
+        }
+        public override string ToString()
+        {
+            return $"{Id} - {Usuario} - {Accion} - {FechaHora} - {Detalles}";
+        }
     }
 }
